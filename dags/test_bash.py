ae734a9f1480a611bc107bc1fdee269958bf58c9
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-09-18 11:30:48 -0400

    updated the dag instantiation

--- dags/test_bash.py
+++ dags/test_bash.py
@@ -6,18 +6,18 @@ from airflow.operators.bash import BashOperator
 default_args = {
     'owner':'nshk'
 }
-dag = DAG(
+with DAG(
     dag_id='simple_bash_test',
     description='Simple Bash Operator',
     default_args = default_args,
     start_date=days_ago(1),
+    schedule_interval=None,
     tags=['bash','example']
-)
+) as dag:
 
-task = BashOperator(
-    task_id = 'simple_bash_task',
-    bash_command='echo Hello World!',
-    dag=dag
-)
+    task = BashOperator(
+        task_id = 'simple_bash_task',
+        bash_command='echo Hello World!'
+    )
 
 task
